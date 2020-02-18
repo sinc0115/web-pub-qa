5da628fb24d58c955c32d82846f4fad4e9818e8e
Katherine <sinc0115@algonquinlive.com>
2020-02-18 15:59:23 -0500

    Simplifies to focus on finding lists ONLY
    Gotta start somewhere! Starting simple, building up.

--- webPubQA.js
+++ webPubQA.js
@@ -1,47 +1,31 @@
-// Grabs the body element
-var text = document.body.textContent
-console.log(text)
-
-// Finds word in text content
-var $makeItBlue = text.match(/lorem/g)
-console.log('Heres your lorems: ' + $makeItBlue)
-
-
-// Highlights all instances of 'lorem'
-//// BROKEN captures '&' in HTML as well, breaking page
-highlight();
-function highlight(){
-    if (document.readyState === "complete") {
-        document.body.innerHTML = document.body.innerHTML.replace(/\&amp;/g, '<span class="highlight">&</span>');
-    } else {
-        setTimeout('highlight();', 500)
-    }
-}
-
-
-
-// Finds all list elements
+// Variables
 const $ulList = document.querySelectorAll('ul')
 const $olList = document.querySelectorAll('ol')
 const $dlList = document.querySelectorAll('dl')
 
-// Gives them a pink border
-for (const $ul of $ulList) {
-	$ul.style.border = "5px solid pink";
+// Finds all list elements
+function findLists($ulList, $olList, $dlList) {
+
+	// Gives them a pink border
+	for (const $ul of $ulList) {
+		$ul.style.border = "5px solid pink";
+	}
+	for (const $ol of $olList) {
+		$ol.style.border = "5px solid pink";
+	}
+	for (const $dl of $dlList) {
+		$dl.style.border = "5px solid hotpink";
+	}
 }
-for (const $ol of $olList) {
-	$ol.style.border = "5px solid pink";
-}
-for (const $dl of $dlList) {
-	$dl.style.border = "5px solid hotpink";
-}
 
+// Records length of all lists
+var $ulListLength = $ulList.length
+console.log('ul Lists: ' + $ulListLength)
+var $olListLength = $olList.length
+console.log('ol Lists: ' + $olListLength)
+var $dlListLength = $dlList.length
+console.log('dl Lists: ' + $dlListLength)
 
-
-// To display number of "lorems" on page, in popup
-/// Needs to be connected to popup, only displays in console right now
-var $loremCount = document.querySelector('.lorem-count')
-// var $loremCount.textContent = document.querySelector('.lorem-count')
-
-$loremCount = $makeItBlue.length
-console.log('Lorem Count: ' + $loremCount)
+// The total number of lists on the page
+$totalListsLength = $ulListLength + $olListLength + $dlListLength
+console.log('Total lists: ' + $totalListsLength)
