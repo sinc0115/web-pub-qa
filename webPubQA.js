df605dca798560adebd70a1677fb43fd9f6e458c
Katherine <sinc0115@algonquinlive.com>
2020-02-19 15:55:12 -0500

    Works! But can't be turned off

--- webPubQA.js
+++ webPubQA.js
@@ -1,31 +1,67 @@
+pinkIt = false
+
 // Variables
 const $ulList = document.querySelectorAll('ul')
 const $olList = document.querySelectorAll('ol')
 const $dlList = document.querySelectorAll('dl')
 
-// Finds all list elements
-function findLists($ulList, $olList, $dlList) {
+function launchExt() {
+	if (pinkIt == false) {
+		// Gives them a pink border
+		for (let $ul of $ulList) {
+			$ul.style.border = "5px solid pink"
+		}
+		for (let $ol of $olList) {
+			$ol.style.border = "5px solid pink"
+		}
+		for (let $dl of $dlList) {
+			$dl.style.border = "5px solid hotpink"
+		}
+	
+		// Records length of all lists
+		var $ulListLength = $ulList.length
+		console.log('ul Lists: ' + $ulListLength)
+		var $olListLength = $olList.length
+		console.log('ol Lists: ' + $olListLength)
+		var $dlListLength = $dlList.length
+		console.log('dl Lists: ' + $dlListLength)
 
-	// Gives them a pink border
-	for (const $ul of $ulList) {
-		$ul.style.border = "5px solid pink";
-	}
-	for (const $ol of $olList) {
-		$ol.style.border = "5px solid pink";
-	}
-	for (const $dl of $dlList) {
-		$dl.style.border = "5px solid hotpink";
-	}
-}
+		// The total number of lists on the page
+		$totalListsLength = $ulListLength + $olListLength + $dlListLength
+		console.log('Total lists: ' + $totalListsLength)
+		return $totalListsLength
+		
+		//Resets values
+		pinkIt = true
+		console.log('Please work!')
+				
+	} else {
+		// Removes border
+		for (let $ul of $ulList) {
+			$ul.style.border = none
+		}
+		for (let $ol of $olList) {
+			$ol.style.border = none
+		}
+		for (let $dl of $dlList) {
+			$dl.style.border = none
+		}
 
-// Records length of all lists
-var $ulListLength = $ulList.length
-console.log('ul Lists: ' + $ulListLength)
-var $olListLength = $olList.length
-console.log('ol Lists: ' + $olListLength)
-var $dlListLength = $dlList.length
-console.log('dl Lists: ' + $dlListLength)
+		// Removes length of all lists
+		var $ulListLength = 0
+		var $olListLength = 0
+		var $dlListLength = 0
 
-// The total number of lists on the page
-$totalListsLength = $ulListLength + $olListLength + $dlListLength
-console.log('Total lists: ' + $totalListsLength)
+		// The total number of lists on the page
+		$totalListsLength = 0
+		return $totalListsLength
+		
+		// Resets values 
+		pinkIt = false
+		console.log('Big nope.')
+		}
+	}
+	
+browser.runtime.onConnect.addListener(launchExt)
+
+
